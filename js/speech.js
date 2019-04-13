10e195b2d2da038bc5facd9a64004f7a0e2e548e
Lab230K <46098114+Lab230K@users.noreply.github.com>
2019-04-14 06:17:21 +0900

    Update speech.js

--- js/speech.js
+++ js/speech.js
@@ -1,8 +1,10 @@
-Tesseract.recognize("./test.png",{lang:"jpn"}).then(function(result){
-    const a = document.querySelector("#test");
-    a.innerHTML = result.html;
-})
-Tesseract.recognize("./test2.png").then(function(result){
-    const a = document.querySelector("#test2");
-    a.innerHTML = result.html;
-})
+
+/**
+ * 
+ */
+function say(something) {
+  dialogueBox.value = something;
+  speechSynthesis.speak(
+    new SpeechSynthesisUtterance(something)
+  );
+}
