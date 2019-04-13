e25aea667a2641da238c892e3c049c845edae4b9
Lab230K <46098114+Lab230K@users.noreply.github.com>
2019-04-14 07:53:51 +0900

    Update test.js

--- Tesserat.js Tutorial/test.js
+++ Tesserat.js Tutorial/test.js
@@ -1,5 +1,5 @@
 Tesseract.recognize("./test.png",{lang:"jpn"}).then(function(result){
-    const a = document.querySelector("#test");
+    const a = document.querySelector("#test1");
     a.innerHTML = result.html;
 })
 Tesseract.recognize("./test2.png").then(function(result){
